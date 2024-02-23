96c13c8ab5ff5ae87c879663289548c81ead7c64
therosemark <160242395+therosemark@users.noreply.github.com>
2024-02-23 13:42:35 +0100

    Add files via upload

--- toolbox.py
+++ toolbox.py
@@ -10,31 +10,27 @@ def secante(f ,x0, x1, tol):
     statut = 0      #initialisation par defaut, aucune erreur
     y0 = f(x0)
     y1 = f(x1)
-    resultat = [None, None]
+    values = []
+    #il reste le cas ou y1*y2 = 0
 
-  #il reste le cas ou x1>x0, et si y1*y2 = 0
-
-     #cas ou x1>x0
-    if x1>x0:
-         a = x0
-         x0 = x1
-         x1 = a
-    
+    #cas ou x1>x0
+    if x1<x0:
+        print("size issue")
+        x0, x1 = x1, x0
+         
     #cas ou x0 est une racine
-    elif abs(y0) < tol : 
-        racines = [x0 , statut]
-        return racines
+    if abs(y0) < tol : 
+        return [x0 , statut]
     
     #cas ou x1 est une racine
-    elif abs(y1) < tol : 
-        racines = [x1 , statut]
-        return racines
+    elif abs(y1) < tol :
+        return [x1 , statut]
     
     #cas ou il n'y aucune racine (deux ordonnees sont du meme signe)
-    elif y0*y1 > 0 :
+    elif y0*y1 > 0 :    
         statut = -1
         erreur = "Il n'y a aucune racine dans cet intervalle"
-        return [print(erreur), statut]
+        return [erreur, statut]
          
 
     #cas ou il y a une racine (au moins une des deux ordonnees est negative)
@@ -48,11 +44,10 @@ def secante(f ,x0, x1, tol):
             y0 = f(x0)
             x1 = x_suiv
             y1 = f(x1)
+            values.append(x0)
         
             if abs(y0) < tol:
-                resultat = [x0, statut]
-                return print(resultat) #statut = 0 par defaut
-                break 
+                return [x0, statut], values #statut = 0 par defaut
             
             
                     
