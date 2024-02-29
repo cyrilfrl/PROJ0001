7cebc5ab3fc7403c22ea3e6c2434d49c4b887339
bebel <100097548+cyrilfrl@users.noreply.github.com>
2024-02-29 22:40:43 +0100

    Add files via upload

--- RechercheRacine.py
+++ RechercheRacine.py
@@ -52,12 +52,9 @@ def bissection(f, x0, x1, tol):
 
 def secante(f ,x0, x1, tol):
     statut = 0 # initialisation
+
     y0 = f(x0)
     y1 = f(x1)
-
-    #cas ou x1>x0
-    if x1<x0:
-        x0, x1 = x1, x0
          
     #cas ou x0 est une racine
     if abs(y0) < tol : 
@@ -67,22 +64,42 @@ def secante(f ,x0, x1, tol):
     elif abs(y1) < tol :
         return [x1 , statut]
     
+    if y0*y1 > 0:
+        erreur = "ErrorMessage: no root in the function"
+        statut = 1
+        return [erreur, statut]
+    
     # calcul du nombre max d'itérations
-    N = math.ceil(math.log((x1 - x0)/(2*tol), math.e)/math.log(2, math.e))
+    N = math.ceil(math.log((abs(x1 - x0))/(tol), math.e)/math.log(2))
     k = 0
-    while k <= N:
+    while k <= (N+1):        
         k += 1
         #si x0 est inferieur a x1! donc il faut prendre le nouvel intervalle a droite en changeant x0
-        x_suiv = x1 - ( (y1*(x1-x0)) / (y1 - y0) )
+        x2 = x1 - ((y1*(x1-x0)) / (y1 - y0))
         x0 = x1
-        y0 = f(x0)
-        x1 = x_suiv
+        y0 = y1
+        x1 = x2
         y1 = f(x1)
         
-        if abs(y0) < tol:
-            return [x0, statut]
-        
-    if k >= N:
-        erreur = "ErrorMessage: la fonction ne converge pas"
-        statut = -1
-        return [erreur, statut]
+        if abs(y1) < tol:
+            return [x1, statut]
+
+    # did not converge    
+    erreur = "ErrorMessage: la fonction ne converge pas"
+    statut = -1
+    return [erreur, statut]
+    
+    
+import math
+def f(x):
+    return math.cos(x)
+    
+x0 = 1.4
+x1 = 1.8
+print(bissection(f, x0, x1, 0.01))
+print(secante(f, x0, x1, 0.01))
+
+import matplotlib.pyplot as plt
+absi = np.linspace(0, 1.5, 100)
+plt.plot(absi, [f(x) for x in absi])
+#plt.show()
