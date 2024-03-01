bbdb09f65977d23b0b8e671577cae99479f8fdd9
bebel <100097548+cyrilfrl@users.noreply.github.com>
2024-03-01 13:58:16 +0100

    Add files via upload

--- RechercheRacine.py
+++ RechercheRacine.py
@@ -3,19 +3,28 @@ import math
 import numpy as np
 
 def bissection(f, x0, x1, tol):
-    # cas x0 après x1
-    if x1 < x0:
-        x0, x1 = x1, x0
-    
     # status verification (hypotheses)
     statut = 0
     half_ecart = (x1-x0)/2
     y0 = f(x0)
     y1 = f(x1)
+
+    # cas x0 après x1
+    if x1 < x0:
+        x0, x1 = x1, x0
+    
+    # cas fonction décroissante
+    if(y0>y1):
+        x0, x1 = x1, x0
+    
     if y0*y1 > 0:
         erreur = "ErrorMessage: no root in the function"
         statut = 1
         return [erreur, statut]
+
+    elif tol <= 10**(-10):
+        statut = 1
+        return[None,statut]
 
     elif y0*y1 == 0:
         if y0 == 0:
@@ -50,52 +59,52 @@ def bissection(f, x0, x1, tol):
         statut = -1
         return [erreur, statut]
 
-def secante(f ,x0, x1, tol):
-    statut = 0 # initialisation
+def secante(f, x0, x1, tol) :
+    statut = 0
 
+    # Hypothesis verification
     y0 = f(x0)
     y1 = f(x1)
-         
-    #cas ou x0 est une racine
-    if abs(y0) < tol : 
-        return [x0 , statut]
-    
-    #cas ou x1 est une racine
-    elif abs(y1) < tol :
-        return [x1 , statut]
-    
-    if y0*y1 > 0:
-        erreur = "ErrorMessage: no root in the function"
+    k = 0
+
+    if tol <= 10**(-10):
+        erreur = "ErrorMessage: invalid tolerance"
         statut = 1
         return [erreur, statut]
     
-    # calcul du nombre max d'itérations
-    N = math.ceil(math.log((abs(x1 - x0))/(tol), math.e)/math.log(2))
-    k = 0
-    while k <= (N+1):        
-        k += 1
-        #si x0 est inferieur a x1! donc il faut prendre le nouvel intervalle a droite en changeant x0
-        x2 = x1 - ((y1*(x1-x0)) / (y1 - y0))
-        x0 = x1
+    if abs(y0 - y1) <= 10**(-10):
+        erreur = "ErrorMessage: point unique"
+        statut = 1
+        return [erreur, statut]
+    
+    N = int(np.log(abs(x1-x0)/(2*tol)))/(np.log(2)) + 1
+    while abs(y1) > tol :
+        x2 = x1
+        x1 = (x1 - (y1 * (x1-x0))/(y1 - y0))
+        x0 = x2
         y0 = y1
-        x1 = x2
         y1 = f(x1)
         
-        if abs(y1) < tol:
+        if(abs(y0-y1) <= 10**(-10)):
+            erreur = "ErrorMessage: doesn't converge"
+            statut = -1
             return [x1, statut]
+        
+        k += 1
+        if k > N:
+            statut = -1
+            return [x1, statut]
+            
+    statut = 0
+    
+    return [x1, statut]
 
-    # did not converge    
-    erreur = "ErrorMessage: la fonction ne converge pas"
-    statut = -1
-    return [erreur, statut]
-    
-    
 import math
 def f(x):
-    return math.cos(x)
-    
-x0 = 1.4
-x1 = 1.8
+    return x**3 - math.cos(x)
+
+x0 = 0.4
+x1 = 1
 print(bissection(f, x0, x1, 0.01))
 print(secante(f, x0, x1, 0.01))
 
